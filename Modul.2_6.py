b2b0c6711ed1bf528b0be8482fd1a6f9a4588b79
Korben89 <Zlotov1@yandex.ru>
2024-08-15 23:39:03 +0300

    Update Modul.2_6.py

--- Modul.2_6.py
+++ Modul.2_6.py
@@ -4,9 +4,9 @@ def get_matrix(n, m, value):
         a = [value] * m
         matrix.append(a)
     return matrix
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
+result1 = get_matrix(5, 7, 3000)
+result2 = get_matrix(2, 6, 'Python')
+result3 = get_matrix(3, 3, 7)
 print(result1)
 print(result2)
 print(result3)
